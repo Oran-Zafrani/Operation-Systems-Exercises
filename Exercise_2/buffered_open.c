ed723c4985d9db0445c3fe19c7d523d21a2799a8
Oran-Zafrani <oran121242431@gmail.com>
2024-12-14 17:31:32 +0200

    finished all

--- Exercise_2/buffered_open.c
+++ Exercise_2/buffered_open.c
@@ -1,11 +1,11 @@
 #include "buffered_open.h"
 #include <stdio.h>
 #include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-int main() {
-    printf("Buffered open example\n");
-    return 0;
-}
 
 buffered_file_t *buffered_open(const char *pathname, int flags, ...) {
     buffered_file_t *bf = malloc(sizeof(buffered_file_t));
@@ -41,6 +41,8 @@ buffered_file_t *buffered_open(const char *pathname, int flags, ...) {
         perror("open file has failed");
         return NULL;
     }
+
+    return bf;
 }
 
 int buffered_flush(buffered_file_t *bf) {
@@ -52,17 +54,63 @@ int buffered_flush(buffered_file_t *bf) {
     while (to_write > 0) {
 
         if (bf->preappend) {
+            // saving the original offset
+            off_t original_offset = lseek(bf->fd, 0, SEEK_CUR);
+            if (original_offset == (off_t)-1) {
+                perror("lseek has failed");
+                return -1;
+            }
+
+            // extracting the file size for reading the content of the whole file
+            off_t file_size = lseek(bf->fd, 0, SEEK_END);
+            if (file_size == (off_t)-1) {
+                perror("lseek has failed");
+                return -1;
+            }
+
+            char *temp_buffer = malloc(file_size);
+            if (!temp_buffer) {
+                perror("temp malloc for flush has failed");
+                return -1;
+            }
+
+            // Read the whole file to a buffer
             lseek(bf->fd, 0, SEEK_SET);
-            bf->preappend = 0;
+            ssize_t read_bytes = read(bf->fd, temp_buffer, file_size);
+            if (read_bytes == -1) {
+                free(temp_buffer);
+                perror("read has failed");
+                return -1;
+            }
+
+            // Write buffered content of the buffer to the beginning of the file
+            lseek(bf->fd, 0, SEEK_SET);
+            written = write(bf->fd, bf->write_buffer, bf->write_buffer_pos);
+            if ( written == -1) {
+                free(temp_buffer);
+                perror("write has failed");
+                return -1;
+            }
+
+            // writing the original content next
+            if (write(bf->fd, temp_buffer, read_bytes) == -1) {
+                free(temp_buffer);
+                perror("write has failed");
+                return -1;
+            }
+            
+            free(temp_buffer);
+            lseek(bf->fd, original_offset, SEEK_SET);
         }else {
             written = write(bf->fd, bf->write_buffer + total_written, to_write);
             if (written == -1) {
                 perror("write has failed");
                 return -1;
             }
-            total_written += written;
-            to_write -= written;
         }
+
+        total_written += written;
+        to_write -= written;
     }
 
     bf->write_buffer_pos = 0;
@@ -75,6 +123,7 @@ int buffered_flush(buffered_file_t *bf) {
 ssize_t buffered_write(buffered_file_t *bf, const void *buf, size_t count) 
 {
     if (!bf || !buf) {
+        perror("one of the arguments is NULL");
         return -1;
     }
     
@@ -88,6 +137,7 @@ ssize_t buffered_write(buffered_file_t *bf, const void *buf, size_t count)
         // If there is no space left in the buffer, flush it
         if (space_left == 0) {
             if (buffered_flush(bf) == -1) {
+                perror("flush before write has failed");
                 return -1;
             }
             space_left = BUFFER_SIZE;
